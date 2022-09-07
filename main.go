1ce4cff1bcd5b58595226a2a768820fbb643eddf
malik <malikharrison167@gmail.com>
2022-09-07 00:01:41 -0700

    playing with code

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	append(cards, "Six of Spades")
+
+	for i, cards := range cards {
+		fmt.Println(i, cards)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
