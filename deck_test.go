396fa547f98b4bb079487d4d8048fbd1c7a4272b
malik <malikharrison167@gmail.com>
2022-09-30 19:39:47 -0700

    End of tutorial

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,38 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
+
+// test cmd = go test -vet=off
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
-	if len(d) != 16 {
-		t.Errorf("Expected deck length of 20, but got", len(d))
+	if len(d) != 44 {
+		t.Errorf("Expected deck length of 44, but got", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Queen of Clubs" {
+		t.Errorf("Expected last card of Queen of Clubs, but got %v", d[len(d)])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 44 {
+		t.Errorf("expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
