8cb514d255a9b9bb70b9eee9ba9fb39c48b36fa4
malik <malikharrison167@gmail.com>
2022-09-30 18:44:39 -0700

    running test

--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	append(cards, "Six of Spades")
-
-	for i, cards := range cards {
-		fmt.Println(i, cards)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
